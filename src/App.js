7b4cffa62644187d82ce53a4004c6c7a415a553e
Student <student@d17-327e-pc-15.ipa.ii.agh.edu.pl>
2025-12-14 12:14:09 +0100

    commit message

--- src/App.js
+++ src/App.js
@@ -22,12 +22,20 @@ function App() {
         alert(title);
   }
 
-
+  const movies = [
+    {title: "Wall-E"},
+    {title: "Pulp Fiction"},
+    {title: "Matrix"},
+    {title: "1670"},
+];
 
   return (
     <div>
       <h1>My favourite movies to watch</h1>
-      <h2>My favourite movie for today is {title}</h2>
+      <h2>Titles</h2>
+      <ul>
+        {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+      </ul>
       <h3>My favourite movie for today is {title}</h3>
       {
       title.length > 0 && <div>{message}</div>
