f0d58c0e0a5b80154b087bd3e874d59c9a7ce9b7
Student <student@d17-327e-pc-14.ipa.ii.agh.edu.pl>
2026-01-17 12:00:02 +0100

    src/App.js git commit -m src/App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
 import './App.css';
 import {useState} from "react";
+import 'milligram';
+import MovieForm from "./MovieForm";
+import {MoviesList} from "./MoviesList";
 
 function App() {
-    const [title, setTitle] = useState('Wall-E');
-
-    let message;
-    if (title.length < 5) {
-        message = <div>Tutuł jest za krótki. Nagrywają takie filmy?</div>;
-    } else if (title.length < 15) {
-        message = <div>Tytuł jest ekstra, w sam raz na plakat przed kinem!</div>;
-    } else {
-        message = <div>Tytuł jest za długi, nikt tego nie zapamięta.</div>;
-    }
-
-    function handleChange(event) {
-        setTitle(event.target.value);
-    }
-
-    const movies = [
-        {title: "Wall-E"},
-        {title: "Pulp Fiction"},
-        {title: "Matrix"},
-        {title: "1670"},
-    ];
+    const [movies, setMovies] = useState([]);
 
     return (
-        <div>
+        <div className="container">
             <h1>My favourite movies to watch</h1>
-            <h2>Titles</h2>
-            <ul>
-                {movies.map(movie => <li key={movie.title}>{movie.title}</li>)}
-            </ul>
-            <h2>My favourite movie for today is {title}</h2>
-            {title.length > 0 && <div>{message}</div>}
-            <input type="text" value={title} onChange={handleChange}/>
-            <button onClick={() => alert(title)}>Pokaż tytuł filmu</button>
+            <MoviesList movies={movies}/>
+            <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])}/>
         </div>
     );
 }
